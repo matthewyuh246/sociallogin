f859b347ba87ae74d0ad52aa00006b46912fcf60
Yuhri <matthewyuh246@gmail.com>
2025-03-26 23:44:04 +0900

    userのusecase

---
+++ internal/usecase/user_usecase.go
@@ -0,0 +1,63 @@
+package usecase
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/matthewyuh246/socallogin/internal/domain"
+	"github.com/matthewyuh246/socallogin/internal/repository"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type IUserUsecase interface {
+	SignUp(user domain.User) (domain.UserResponse, error)
+	Login(user domain.User) (string, error)
+}
+
+type userUsecase struct {
+	ur repository.IUserRepository
+}
+
+func NewUserUsecase(ur repository.IUserRepository) IUserUsecase {
+	return &userUsecase{ur}
+}
+
+func (uu *userUsecase) SignUp(user domain.User) (domain.UserResponse, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil {
+		return domain.UserResponse{}, err
+	}
+	newUser := domain.User{
+		Email:    user.Email,
+		Password: string(hash),
+	}
+	if err := uu.ur.CreateUser(&newUser); err != nil {
+		return domain.UserResponse{}, err
+	}
+	resUser := domain.UserResponse{
+		ID:    newUser.ID,
+		Email: newUser.Email,
+	}
+	return resUser, nil
+}
+
+func (uu *userUsecase) Login(user domain.User) (string, error) {
+	storedUser := domain.User{}
+	if err := uu.ur.GetUserByEmail(&storedUser, user.Email); err != nil {
+		return "", err
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(storedUser.Password), []byte(user.Password))
+	if err != nil {
+		return "", err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": storedUser.ID,
+		"exp":     time.Now().Add(time.Hour * 12).Unix(),
+	})
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+	if err != nil {
+		return "", nil
+	}
+	return tokenString, nil
+}
