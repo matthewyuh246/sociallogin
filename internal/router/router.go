c92d8e2ca817c2fa3170b818d9b26ac585380a1c
Yuhri <matthewyuh246@gmail.com>
2025-03-30 20:49:11 +0900

    signupをpostからgetに変更

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,15 +1,24 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/matthewyuh246/socallogin/internal/controller"
 )
 
 func NewRouter(uc controller.IUserController) *echo.Echo {
 	e := echo.New()
 
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://your-frontend-domain.com"},
+		AllowMethods:     []string{http.MethodGet, http.MethodPost, http.MethodOptions},
+		AllowCredentials: true,
+	}))
+
 	e.GET("/auth", uc.Authentication)
-	e.POST("/signup", uc.SignUp)
+	e.GET("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
 
