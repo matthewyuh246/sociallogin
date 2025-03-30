46464123fd86f5a735756540b4728b685192aed1
Yuhri <matthewyuh246@gmail.com>
2025-03-30 18:37:42 +0900

    main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"github.com/matthewyuh246/socallogin/internal/controller"
+	"github.com/matthewyuh246/socallogin/internal/repository"
 	"github.com/matthewyuh246/socallogin/internal/router"
+	"github.com/matthewyuh246/socallogin/internal/usecase"
 	db "github.com/matthewyuh246/socallogin/pkg/database"
 )
 
 func main() {
 	db := db.NewDB()
-	e := router.NewRouter()
+	userRepository := repository.NewUserRespository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository)
+	userController := controller.NewUserController(userUsecase)
+	e := router.NewRouter(userController)
 	e.Logger.Fatal(e.Start(":8080"))
 }
