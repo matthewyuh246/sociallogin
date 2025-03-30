8463f044d0aaf941aef58401980980fe0938015b
Yuhri <matthewyuh246@gmail.com>
2025-03-30 17:57:33 +0900

    user_controller

--- internal/controller/user_controller.go
+++ internal/controller/user_controller.go
@@ -5,17 +5,22 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/coreos/go-oidc"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/matthewyuh246/socallogin/internal/domain"
 	"github.com/matthewyuh246/socallogin/internal/usecase"
-	"github.com/x/oauth2"
+	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
 type IUserController interface {
 	Authentication(c echo.Context) error
+	SignUp(c echo.Context) error
+	LogIn(c echo.Context) error
+	LogOut(c echo.Context) error
 }
 
 type userController struct {
@@ -60,4 +65,81 @@ func init() {
 func (uc *userController) Authentication(c echo.Context) error {
 	url := config.AuthCodeURL("state", oauth2.AccessTypeOffline)
 	return c.Redirect(http.StatusFound, url)
+}
+
+func (uc *userController) SignUp(c echo.Context) error {
+	ctx := context.Background()
+	oauth2Token, err := config.Exchange(ctx, c.QueryParam("code"))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "Failed to exchange token: "+err.Error())
+	}
+
+	rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+	if !ok {
+		return c.JSON(http.StatusInternalServerError, "No id_token field in oauth2 token.")
+	}
+
+	idToken, err := provider.Verifier(&oidc.Config{ClientID: clientID}).Verify(ctx, rawIDToken)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "Failed to verify ID Token: "+err.Error())
+	}
+
+	var profile map[string]interface{}
+	if err := idToken.Claims(&profile); err != nil {
+		return c.JSON(http.StatusInternalServerError, "Faile to get user profile: "+err.Error())
+	}
+
+	if domain, ok := profile["hd"].(string); !ok || domain != "g.kogakuin.jp" {
+		return c.JSON(http.StatusUnauthorized, "Unauthorized domain")
+	}
+
+	user := domain.User{}
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	user.Email = profile["email"].(string)
+
+	userRes, err := uc.uu.SignUp(user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusCreated, userRes)
+
+}
+
+func (uc *userController) LogIn(c echo.Context) error {
+	user := domain.User{}
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	tokenString, err := uc.uu.Login(user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = tokenString
+	cookie.Expires = time.Now().Add(24 * time.Hour)
+	cookie.Path = "/"
+	cookie.Domain = os.Getenv("API_DOMAIN")
+	cookie.Secure = true
+	cookie.HttpOnly = true
+	cookie.SameSite = http.SameSiteNoneMode
+	c.SetCookie(cookie)
+	return c.NoContent(http.StatusOK)
+}
+
+func (uc *userController) LogOut(c echo.Context) error {
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = ""
+	cookie.Expires = time.Now()
+	cookie.Path = "/"
+	cookie.Domain = os.Getenv("API_DOMAIN")
+	cookie.Secure = true
+	cookie.HttpOnly = true
+	cookie.SameSite = http.SameSiteNoneMode
+	c.SetCookie(cookie)
+	return c.NoContent(http.StatusOK)
 }
